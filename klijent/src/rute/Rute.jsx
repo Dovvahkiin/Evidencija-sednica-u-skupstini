9f4495f5a9feab99c5474e635605c92243392849
Božidar Tovarnicki <bozidartovarnickii@gmail.com>
2025-09-13 21:28:26 +0200

    Added some routes

--- klijent/src/rute/Rute.jsx
+++ klijent/src/rute/Rute.jsx
@@ -3,6 +3,9 @@ import { createBrowserRouter } from 'react-router-dom'
 
 // stranice
 import Pocetna from "../stranice/Pocetna"
+import Prijava from "../stranice/Prijava"
+import Profil from "../stranice/Profil"
+import Registracija from "../stranice/Registracija"
 import NijePronadjeno from "../stranice/NijePronadjeno"
 
 //funkcija routera
@@ -12,7 +15,18 @@ const ruter = createBrowserRouter([
         path: "/",
         element: <Pocetna/>,
         errorElement: <NijePronadjeno/>
-    }
+    },
+    {
+        path: "/Prijava",
+        element: <Prijava />
+    },    {
+        path: "/Profil",
+        element: <Profil />
+    },    {
+        path: "/Registracija",
+        element: <Registracija />
+    },
+
 ])
 
 export default ruter
