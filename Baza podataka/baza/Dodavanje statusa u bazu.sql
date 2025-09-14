f7dbf9ddc69efc38b65a428c142a9fb9834b2d09
Božidar Tovarnicki <bozidartovarnickii@gmail.com>
2025-09-14 21:19:26 +0200

    Added users tables and creating statuses sql

---
+++ Baza podataka/baza/Dodavanje statusa u bazu.sql
@@ -0,0 +1,5 @@
+insert into evidencijasednica.status_korisnika (NazivStatusa) values ('admin');
+insert into evidencijasednica.status_korisnika (NazivStatusa) values ('korisnik');
+
+insert into evidencijasednica.status_sednice (NazivStatusaSednice) values ('zakazana');
+insert into evidencijasednica.status_sednice (NazivStatusaSednice) values ('zavrsena');
--- Baza podataka/baza/Kreiranje tabela.sql
+++ Baza podataka/baza/Kreiranje tabela.sql
@@ -1,6 +1,8 @@
 DROP TABLE IF EXISTS evidencijasednica.dnevni_red;
 DROP TABLE IF EXISTS evidencijasednica.sednica;
 DROP TABLE IF EXISTS evidencijasednica.status_sednice;
+DROP TABLE IF EXISTS evidencijasednica.korisnici;
+DROP TABLE IF EXISTS evidencijasednica.status_korisnika;
 
 create table if not exists evidencijasednica.status_sednice(
 IDStatusaSednice int auto_increment primary key,
@@ -23,4 +25,19 @@ RedniBrojTacke int not null,
 TekstTacke text not null,
 SednicaID int not null,
 foreign key (sednicaID) references evidencijasednica.sednica(IDSednice) on delete cascade
+);
+
+create table if not exists evidencijasednica.status_korisnika(
+IDStatusaKorisnika int auto_increment primary key,
+NazivStatusa varchar(20) not null
+);
+
+create table if not exists evidencijasednica.korisnici(
+IDKorisnika int auto_increment primary key,
+ImeKorisnika varchar(50) not null,
+PrezimeKorisnika varchar(50) not null,
+EmailKorisnika varchar(150) not null,
+LozinkaKorisnika varchar(30) not null,
+IDStatusa int not null,
+foreign key (IDStatusa) references evidencijasednica.status_korisnika (IDStatusaKorisnika)
 );
