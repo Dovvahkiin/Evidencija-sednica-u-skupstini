6db2113620aab0e4555fac099102d362292ac3d8
Božidar Tovarnicki <bozidartovarnickii@gmail.com>
2025-09-16 01:26:20 +0200

    Added procedure for deleting a meeting

---
+++ Baza podataka/procedure/obrisiSednicu.sql
@@ -0,0 +1,28 @@
+delimiter $$
+
+create procedure obrisiSednicu (in IDSedniceParametar int)
+izadji:
+
+begin
+	declare sednicaProvera int;
+
+start transaction;
+
+	select IDSednice into sednicaProvera
+	from evidencijasednica.sednica s
+	where s.IDSednice = IDSedniceParametar
+	limit 1;
+	
+	if sednicaProvera is null or sednicaProvera = 0
+	then
+	rollback;
+	select concat ('Sednica ne postoji. Transakcija nije uspela.') as greska;
+	leave izadji;
+	end if;
+	
+	delete from evidencijasednica.sednica s where s.IDSednice = IDSedniceParametar;
+	commit;
+	select concat('Uspesno obrisana sednica. Transakcija uspesna.') as uspesno;
+	
+end $$
+delimiter ;
