cb73526af0a05031ee3a9c425dbe2f85a118000c
Božidar Tovarnicki <bozidartovarnickii@gmail.com>
2025-09-16 02:21:19 +0200

    Added view for user profile

---
+++ Baza podataka/pogledi/pregledProfilaKorisnika.sql
@@ -0,0 +1,7 @@
+create view pregledProfilaKorisnika as
+select korisnik.ImeKorisnika as ImeKorisnika,
+	   korisnik.PrezimeKorisnika as PrezimeKorisnika,
+	   korisnik.EmailKorisnika as EmailKorisnika,
+	   status.NazivStatusa as StatusKorisnika
+from evidencijasednica.korisnici korisnik
+join evidencijasednica.status_korisnika status on status.IDStatusaKorisnika = korisnik.IDStatusa
