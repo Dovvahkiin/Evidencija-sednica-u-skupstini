28adc0ba2335397cb1476ea0ed451d2eb31be6f5
Božidar Tovarnicki <bozidartovarnickii@gmail.com>
2025-09-20 22:06:52 +0200

    Added new meeting types

--- Baza podataka/baza/Dodavanje statusa u bazu.sql
+++ Baza podataka/baza/Dodavanje statusa u bazu.sql
@@ -1,2 +1,2 @@
 insert into evidencijasednica.status_korisnika (NazivStatusa) values ('admin'),('korisnik');
-insert into evidencijasednica.status_sednice (NazivStatusaSednice) values ('zakazana'),('zavrsena');
+insert into evidencijasednica.status_sednice (NazivStatusaSednice) values ('zakazana'),('zavrsena'), ('odlozena'),('otkazana');
