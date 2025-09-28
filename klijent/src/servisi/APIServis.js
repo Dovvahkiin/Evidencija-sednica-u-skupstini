5a676050f0ac1f6c9defa3dbbba6457b2fd47996
Božidar Tovarnicki <bozidartovarnickii@gmail.com>
2025-09-28 08:19:58 +0200

    Vercel config

--- klijent/src/servisi/APIServis.js
+++ klijent/src/servisi/APIServis.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+const nodePort = import.meta.env.NODE_PORT;
 
 class KoriscenjeAxiosa {
   constructor() {
     this.api = axios.create({
-      baseURL: "http://localhost:3000",
+      baseURL: `http://localhost:${nodePort}`,
       withCredentials: true,
     });
   }
