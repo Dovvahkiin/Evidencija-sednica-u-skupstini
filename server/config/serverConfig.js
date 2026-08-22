4cdfb07a6edb349285ed7681c20686f0c1b0567d
Dovvahkiin <bozidartovarnickii@gmail.com>
2026-08-22 16:23:42 +0200

    Refactored config files

---
+++ server/config/serverConfig.js
@@ -0,0 +1,10 @@
+export const nodePort = process.env.NODE_PORT;
+export const clientPort = process.env.KLIJENT_PORT;
+
+export const serverConfig = {
+  corsOptions: {
+    origin: [`http://localhost:${clientPort}`],
+    method: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  },
+};
--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 require("dotenv").config("./.env");
-const chalk = require("chalk");
+import chalk from "chalk";
 
 const app = require("./app.js");
 const serverKonfiguracija = require("./konfiguracije/serverKonfiguracija");
 
 app.listen(serverKonfiguracija.nodePort, () =>
   console.log(
-    chalk.blue(`Example app listening on port ${serverKonfiguracija.nodePort}!`)
-  )
+    chalk.blue(
+      `Example app listening on port ${serverKonfiguracija.nodePort}!`,
+    ),
+  ),
 );
