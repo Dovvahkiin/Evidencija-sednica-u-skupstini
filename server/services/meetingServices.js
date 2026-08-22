5e8fee4b5b571e558e3bc7510fdd8f9206a63e11
Dovvahkiin <bozidartovarnickii@gmail.com>
2026-08-22 19:46:42 +0200

    Created meeting services class

---
+++ server/services/meetingServices.js
@@ -0,0 +1,68 @@
+import DBModel from "../models/dbModel";
+import MeetingModel from "../models/meetingModel";
+import MembersClass from "../utils/quota";
+import chalk from "chalk";
+
+class MeetingServices {
+  getAllMeetings = async () => {
+    const getMeetings = new DBModel("detaljanpregledsednice");
+    const meetings = await getMeetings.getAll();
+
+    if (meetings.length === 0) {
+      console.log(chalk.red("There is no recored meetings."));
+    } else {
+      console.log(chalk.green("List of meetings:\n"));
+      console.log(meetings);
+    }
+    return meetings;
+  };
+
+  getMeetingById = async (id) => {
+    const getMeeting = new DBModel("detaljanpregledsednice");
+    const meeting = await getMeeting.getById(id);
+
+    if (meeting.length === 0) {
+      console.log(chalk.red("There is no recored meeting with that ID."));
+    } else {
+      console.log(chalk.green("Selected meeting:\n"));
+      console.log(meeting);
+    }
+    return meeting;
+  };
+
+  createMeeting = async (data) => {
+    const [meetingTitle, date, attendees, meetingStatusId, content] = data;
+    let newStatus = meetingStatusId;
+    const currentMembersNumber = await MembersClass.membersCheck(attendees);
+    if (currentMembersNumber === 0) newStatus = 3;
+
+    return MeetingModel.createMeeting(
+      meetingTitle,
+      date,
+      attendees,
+      newStatus,
+      content,
+    );
+  };
+
+  deleteMeeting = async (id) => {
+    const deleteResult = await MeetingModel.deleteMeeting(id);
+    if (deleteResult > 0) {
+      return true;
+    } else return false;
+  };
+
+  editMeeting = async (data, id) => {
+    const [meetingTitle, date, attendees, meetingStatusId, content] = data;
+    return MeetingModel.editMeeting(
+      id,
+      meetingTitle,
+      date,
+      attendees,
+      meetingStatusId,
+      content,
+    );
+  };
+}
+
+export default MeetingServices;
