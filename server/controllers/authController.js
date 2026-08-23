1d3032b2c7572ff63ca3f4fa56177697f4d4d8cf
Dovvahkiin <bozidartovarnickii@gmail.com>
2026-08-23 17:02:37 +0200

    Refactored authentication controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,15 +1,32 @@
 import JwtService from "../services/jwtService";
 import { genericCatchBlock } from "../utils/constants";
 import UserValidations from "../validations/userValidations";
+import { setCookies, deleteCookies } from "../services/cookieServices";
 
 class AuthenticationController {
   login = async (req, res) => {
     try {
       const data = req.body;
       const validationResult = await UserValidations.loginValidation(data);
+      if (validationResult.length > 0)
+        return res.status(400).json({ errors: validationResult.errors });
+
+      const tokens = await JwtService.login(data);
+      setCookies(res, tokens);
+
+      return res
+        .status(200)
+        .json({ success: true, message: "User is logged in!", tokens });
     } catch (error) {
       return genericCatchBlock(res, error);
     }
+  };
+
+  logout = (req, res) => {
+    deleteCookies();
+    return res
+      .status(200)
+      .json({ success: true, message: "You are successfully logged out!" });
   };
 }
 
