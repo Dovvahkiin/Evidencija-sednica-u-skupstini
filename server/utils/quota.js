7a4a2d141c68edf2f36feaf4ad00024afcbae526
Dovvahkiin <bozidartovarnickii@gmail.com>
2026-08-23 18:31:14 +0200

    Fixed old way of file path

--- server/utils/quota.js
+++ server/utils/quota.js
@@ -1,20 +1,24 @@
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const membersInput = path.join(__dirname, "members.json");
 
-const quota = async () => {
-  const totalNumberOfMembers = await MembersClass.loadNumberOfMembers();
+const quota = () => {
+  const totalNumberOfMembers = MembersClass.loadNumberOfMembers();
   const result = Math.floor((totalNumberOfMembers * 50) / 100 + 1);
   return result;
 };
 
 class MembersClass {
-  static loadNumberOfMembers = async () => {
+  static loadNumberOfMembers = () => {
     const result = JSON.parse(fs.readFileSync(membersInput, "utf8"));
     return result.numberOfMembers;
   };
   static membersCheck = async (data) => {
-    const isThereEnoughMembers = await quota();
+    const isThereEnoughMembers = quota();
     if (data >= isThereEnoughMembers) return true;
     else return false;
   };
