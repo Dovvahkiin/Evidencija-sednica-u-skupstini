32eff3f75112f3c49b5e5de9db9351dea4a527a1
Dovvahkiin <bozidartovarnickii@gmail.com>
2026-08-25 00:37:23 +0200

    Used dirname from helper.js

--- server/utils/quota.js
+++ server/utils/quota.js
@@ -1,9 +1,6 @@
 import fs from "fs";
 import path from "path";
-import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+import { __dirname } from "./helper.js";
 const membersInput = path.join(__dirname, "members.json");
 
 const quota = () => {
