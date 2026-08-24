77d81cdeb7c12a41e191db66a2e0ec125dfe1303
Dovvahkiin <bozidartovarnickii@gmail.com>
2026-08-24 17:35:52 +0200

    Fixed getbyid by creating instance of the class

--- server/services/userServices.js
+++ server/services/userServices.js
@@ -1,9 +1,9 @@
 import DBModel from "../models/dbModel.js";
-import UserModel from "../models/userModel.js";
+const profile = new DBModel("userprofile");
 
 class UserServices extends DBModel {
   static getUserById = async (id) => {
-    return UserModel.getById(id);
+    return profile.getById(id);
   };
 }
 
