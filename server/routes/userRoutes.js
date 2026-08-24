289ed6af376abc39930281b661724a051429d93f
Dovvahkiin <bozidartovarnickii@gmail.com>
2026-08-24 17:35:23 +0200

    Changed http function

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 import AuthMiddleware from "../middleware/auth.js";
 import UserController from "../controllers/userController.js";
 
-router.post(
+router.get(
   "/profile",
   AuthMiddleware.Authentication,
   UserController.getProfile,
