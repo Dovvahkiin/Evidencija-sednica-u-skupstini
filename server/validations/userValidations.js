25cbdfb22915719810fd65318403379b251633f9
Dovvahkiin <bozidartovarnickii@gmail.com>
2026-08-24 15:08:08 +0200

    fixed email regex validation

--- server/validations/userValidations.js
+++ server/validations/userValidations.js
@@ -7,7 +7,7 @@ class UserValidations {
 
     if (!email || email.trim() === "") errors.push("Email cannot be empty!\n");
 
-    if (emailRegex.test(email)) errors.push("Invalid email address!\n");
+    if (!emailRegex.test(email)) errors.push("Invalid email address!\n");
 
     if (!password || password.trim() === "" || password.length < 8)
       errors.push("Password must be at least 8 characters long!\n");
