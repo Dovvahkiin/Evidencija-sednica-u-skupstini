76731b67e3040719ccaf2268c5ea40eeef5f4444
Dovvahkiin <bozidartovarnickii@gmail.com>
2026-08-26 01:22:52 +0200

    Test demo

--- client/src/pages/demo.jsx
+++ client/src/pages/demo.jsx
@@ -1,9 +1,11 @@
-import Header from "../components/layouts/Header/Header";
-
+import Header from "@/components/layouts/Header/Header";
+import Footer from "@/components/layouts/Footer/Footer";
 const Demo = () => {
   return (
     <>
       <Header />
+      <div style={{ height: "100vh" }} />
+      <Footer />
     </>
   );
 };
