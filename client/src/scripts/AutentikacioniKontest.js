331f611deb680d4af28bafd00d5a50de53e375c5
Božidar Tovarnicki <bozidartovarnickii@gmail.com>
2026-08-25 13:13:41 +0200

    Delete client directory

--- client/src/scripts/AutentikacioniKontest.js
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from "react";
-
-export const AutentikacioniKontekst = createContext(null);
-
-export const useAutentikacija = () => {
-  return useContext(AutentikacioniKontekst);
-};
